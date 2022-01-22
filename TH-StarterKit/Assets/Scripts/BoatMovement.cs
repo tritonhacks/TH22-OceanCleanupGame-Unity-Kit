f1787417b022ff558d457c994c780d665c641c93
Will_Dev <97367484+w2llS@users.noreply.github.com>
2022-01-21 20:26:18 -0700

    Basic movement

---
+++ TH-StarterKit/Assets/Scripts/BoatMovement.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatMovement : MonoBehaviour
+{
+    [Header("Components")]
+    public Rigidbody rb;
+
+    [Header("Movement")]
+    public float speed;
+    public float rotation_speed;
+    public float drag;
+
+    [Header("Raycast")]
+    public LayerMask floorLayer;
+    private Vector3 targetPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //drag
+        rb.drag = drag;
+
+
+        //Raycast from mouse
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 100f, floorLayer))
+        {
+            targetPos = hit.point;
+        }
+
+        float distance = Vector3.Distance(transform.position, targetPos);
+        Vector3 direction = (targetPos - transform.position).normalized;
+
+        if (distance > 2f)
+        {
+            //rotation
+            Quaternion rot = Quaternion.LookRotation(direction);
+            Quaternion newrot = Quaternion.Euler(0, rot.eulerAngles.y, 0); //ensure rotation only happens in y axis
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, newrot, rotation_speed * Time.deltaTime);
+
+            //movement
+            rb.AddForce(transform.forward * speed);
+
+        }
+    }
+}
